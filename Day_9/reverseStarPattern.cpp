02f0883d238b6821fe1b323b5e1c1fcb6a068a00
Vishal_dev <25121cn044@glbitm.ac.in>
2026-06-17 13:55:56 +0530

    Create reverseStarPattern.cpp

---
+++ Day_9/reverseStarPattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,i,j;
+    cout<<"Enter no of rows:";
+    cin>>m;
+    for(i=m;i>=0;i--){
+        for(j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
