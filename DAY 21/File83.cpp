16fd441f5b059b3e7742f053f93b73ea1430ae8a
Vishalkgupta <25121cn044@glbitm.ac.in>
2026-06-21 13:04:22 +0530

    DAY 21

---
+++ DAY 21/File83.cpp
@@ -0,0 +1,31 @@
+//Write a program to Count vowels and consonants.
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if ((str[i] >= 'A' && str[i] <= 'Z') ||
+            (str[i] >= 'a' && str[i] <= 'z')) {
+
+            if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+                str[i] == 'O' || str[i] == 'U' || str[i] == 'a' ||
+                str[i] == 'e' || str[i] == 'i' || str[i] == 'o' ||
+                str[i] == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    cout << "Vowels = " << vowels << endl;
+    cout << "Consonants = " << consonants << endl;
+
+    return 0;
+}
